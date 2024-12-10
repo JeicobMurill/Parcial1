0527b33a42fa279784d8f7e19f5ad85d14713617
JeicobMurill <145068123+JeicobMurill@users.noreply.github.com>
2024-12-10 18:11:04 -0500

    Parcial1.java

--- Parcial1/src/parcial1/Parcial1.java
+++ Parcial1/src/parcial1/Parcial1.java
@@ -7,51 +7,52 @@ package parcial1;
 
 import java.util.Scanner;
 
-
 public class Parcial1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("                Bienvenido al Restaurante UNAL");
-        
+
         boolean salir = false;
         while (!salir) {
             // Menú de opciones
-            System.out.println("\n--- Que desdea hacer a continuacion? ---");
+            System.out.println("\n--- ¿Que desea hacer a continuacion? ---");
             System.out.println("1. Ver Menu Disponible");
             System.out.println("2. Realizar Pedido");
             System.out.println("3. Salir");
             System.out.print("Seleccione una opcion: ");
             int opcion = scanner.nextInt();
-            
+
             switch (opcion) {
                 case 1:
-                    System.out.println("                Menu del dia");
-                    System.out.println("Para los Platos fuertes tenemos disponible las siguientes opciones:");
-                    System.out.println("Pasta Bolognesa");
-                    System.out.println("Churrasco");
-                    System.out.println("Hamburguesa Veggie");
-                    System.out.println("Para los Acompanantes tenemos disponible las siguientes opciones:");
-                    System.out.println("Ensalada Cesar");
-                    System.out.println("Porcion de Papas Fritas");
-                    System.out.println("Platano Aborrajado");
-                    System.out.println("Para Bebidas tenemos disponible las siguientes opciones:");
-                    System.out.println("Gaseosa Coca-Cola");
-                    System.out.println("Jugo natural de Mora");
-                    System.out.println("Soda de Sandia");
+                    mostrarMenu();
                     break;
                 case 2:
                     realizarPedido pedido = new realizarPedido();
+                    pedido.tomarPedido();
                     break;
                 case 3:
-                    System.out.println("Hasta una proxima oportunidad!...");
+                    System.out.println("¡Hasta una proxima oportunidad!");
                     salir = true;
                     break;
                 default:
-                    System.out.println("Opcion invalida. Intente nuevamente.");
+                    System.out.println("Opcion inválida. Intente nuevamente.");
             }
-        
-    
         }
     }
+
+    public static void mostrarMenu() {
+        System.out.println("\n                Menu del Día");
+        System.out.println("Platos Fuertes:");
+        System.out.println("1. Pasta Bolognesa - $25,000");
+        System.out.println("2. Churrasco - $30,000");
+        System.out.println("3. Hamburguesa Veggie - $35,000");
+        System.out.println("Acompanantes:");
+        System.out.println("1. Ensalada Cesar - $15,000");
+        System.out.println("2. Porcion de Papas Fritas - $10,000");
+        System.out.println("3. Platano Aborrajado - $25,000");
+        System.out.println("Bebidas:");
+        System.out.println("1. Gaseosa Coca-Cola - $5,000");
+        System.out.println("2. Jugo Natural de Mora - $7,000");
+        System.out.println("3. Soda de Sandia - $8,000");
+    }
 }
-    
